ae4a1997c7f5d4f20f46e17cdd9c0e5bc361691d
Vladimir <vnstrokov@gmail.com>
2024-06-20 18:24:20 -0400

    var mass chirps

--- gw-sounds.py
+++ gw-sounds.py
@@ -79,19 +79,17 @@ Msun *= G/c**3
 # %%
 # generate a GW sound of length [s] for equal-mass binaries of masses mm [Msun] at frequencies ff [Hz] 
 
-def gen_sounds(duration,ff,mm, sr=44100):
+def gen_sounds(duration,ff,mm, sr=44100, fmin=1e+2, fmax=4e+3):
     
     length = int(sr*duration)
     
     numsignals = len(ff)
     
     eta = 0.25
-    Mc = eta**(3./5) * mm
+    Mc = eta**(3./5) * mm * 2
     Mc *= Msun
     
     tc = 5*Mc/256. * (np.pi*Mc*ff)**(-8./3)
-    
-    ff = 100*(ff/1e-4)**(np.log10(40)/2)
     tc /= sr
     
     print(np.sort(tc)[:10])
@@ -101,7 +99,6 @@ def gen_sounds(duration,ff,mm, sr=44100):
     tmerge = np.tile(tc, (length,1)).T
     mchirp = np.tile(eta**(3./5) * mm, (length,1)).T
     psi = np.tile(2*np.pi*rng.random(numsignals), (length,1)).T
-    A = 1.
     
     
     times = np.linspace(0,duration,length)
@@ -112,34 +109,71 @@ def gen_sounds(duration,ff,mm, sr=44100):
         (1-times/tmerge),
         1.
     )
-
     
+    cond_freq = np.logical_and(
+        freq*factor**(-3./8) < 1e-2, 
+        freq*factor**(-3./8) < 4400/(mchirp/(2*eta**(3./5)))
+    )
+    cond = np.logical_and(cond, cond_freq)
+    
+    freq = fmin*(freq/1e-4)**(np.log10(fmax/fmin)/2)
+
     phase = 2*np.pi*freq * (8./5)*tmerge
     phase *=(1 - factor**(5./8))
     
+    ampl = np.where(
+        cond,
+        mchirp**(5./3)*(freq*factor**(-3./8))**(2./3),
+        0.
+    )
     
-    signal = np.sum(mchirp**(5./3)*(freq*factor**(-3./8))**(2./3)*np.cos(phase + psi), axis=0)
     
-    amplitude = np.iinfo(np.int16).max
-    
-    return times, amplitude * signal/np.abs(signal).max()
+    return times, np.sum(ampl*np.cos(phase + psi), axis=0)
 
 
 
 # %%
 rng = np.random.default_rng()
 
-numsignals = 100
+numsignals = 12
 samplerate = 2*44100
 
-ff = 10**(-4 + 2*rng.random(numsignals))
-mm = 0.5 + rng.random(numsignals)
+mm = np.logspace(3,7,5)
+ff = np.full_like(mm, 1e-3)
+duration = np.full_like(mm, 5.)
 
-times,data = gen_sounds(5.3, ff, mm, sr=samplerate)
+chunks = []
+times = []
+shift = 0.
+
+amplitude = np.iinfo(np.int16).max
+
+for f,m,dur in zip(ff,mm,duration):
+    f = np.array([f])
+    m = np.array([m])
+    time,chunk = gen_sounds(dur, f, m, sr=samplerate)
+    
+    if np.abs(chunk).max() != 0.:
+        chunk = amplitude * chunk/np.abs(chunk).max()
+    
+    times.append(time + shift)
+    chunks.append(chunk)
+    
+    shift += dur
+
+signal = np.concatenate(chunks)
+time = np.concatenate(times)
+
+#data = amplitude * signal/np.abs(signal).max()
+data = signal
+
+# %%
+# amplitude = np.iinfo(np.int16).max
+# data = amplitude * signal/np.abs(signal).max()
 
 fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,5))
 
-ax.plot(times, data)
+ax.plot(time, data)
 
 
 ax.grid(True,linestyle=':',linewidth='1.')
@@ -153,6 +187,16 @@ ax.set_ylabel('amplitude')
 # %%
 from scipy.io.wavfile import write
 
+
 write("example.wav", samplerate, data.astype(np.int16))
+
+# %%
+fmin = 100
+fmax = 4000
+
+fmin*(1e-3/1e-4)**(np.log10(fmax/fmin)/2)
+
+# %%
+chunk
 
 # %%
